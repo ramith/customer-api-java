7655c49765322713fe64ce799d740e2fd378baa7
Ramith Jayasinghe <ramith@wso2.com>
2023-02-06 13:01:02 +0530

    Adding logging

--- src/main/java/com/wso2/choreo/demo/CustomerRestController.java
+++ src/main/java/com/wso2/choreo/demo/CustomerRestController.java
@@ -2,6 +2,8 @@ package com.wso2.choreo.demo;
 
 import com.wso2.choreo.demo.model.Customer;
 import com.wso2.choreo.demo.repo.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +15,15 @@ import java.util.Optional;
 @RequestMapping(path = "/customer")
 public class CustomerRestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomerRestController.class);
     @Autowired
     private CustomerRepository customerRepository;
 
     @GetMapping(path = "/{accountId}")
     public @ResponseBody Customer findCustomerById(@PathVariable("accountId") String accountId) {
+
+        logger.info("finding customer by id", accountId);
+
         Optional<Customer> customerOptional = customerRepository.findById(accountId);
         if (customerOptional.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "customer not found");
